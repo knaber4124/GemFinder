ee9492ad964842d09038938609c094f77d4210ac
Keith Naber <knaber4124@gmail.com>
2019-07-16 22:35:57 -0400

    logic for buttons and win/loss

--- gem-game.js
+++ gem-game.js
@@ -1,32 +1,105 @@
+$(document).ready(function(){
+
 //Global Arrays
-let randNumberString=[Math.floor(Math.random()*31)+30];
-let playerNumber=0;
-let wins=0;
-let losses=0;
-let randNumber=parseInt(randNumberString);
-let imageNumber1start=[Math.floor(Math.random()*11)+1];
-let imageNumber2start=[Math.floor(Math.random()*11)+1];
-let imageNumber3start=[Math.floor(Math.random()*11)+1];
-let imageNumber4start=[Math.floor(Math.random()*11)+1];
-let imageNumber1=parseInt(imageNumber1start)+1;
-let imageNumber2=parseInt(imageNumber2start);
-let imageNumber3=parseInt(imageNumber3start);
-let imageNumber4=parseInt(imageNumber4start)+1;
+let randNumberString = [Math.floor(Math.random() * 31) + 30];
+let playerNumberString = 0;
+let wins = 0;
+let losses = 0;
+let randNumber = parseInt(randNumberString);
+let imageNumber1string = [Math.floor(Math.random() * 11) + 1];
+let imageNumber2string = [Math.floor(Math.random() * 11) + 1];
+let imageNumber3string = [Math.floor(Math.random() * 11) + 1];
+let imageNumber4string = [Math.floor(Math.random() * 11) + 1];
+let playerNumber = parseInt(playerNumberString);
 
-
-
+//Assign Numbers To Images
+let imageNumber1 = parseInt(imageNumber1string);
+let imageNumber2 = parseInt(imageNumber2string);
+let imageNumber3 = parseInt(imageNumber3string);
+let imageNumber4 = parseInt(imageNumber4string);
 
 //DOM Manipulation
 $('.yourNumber').text(playerNumber);
 $('.selectedNumber').text(randNumber);
+$('.wins').text(wins);
+$('.losses').text(losses);
 
-
-
-
-//Assign Numbers To Images
-
+//Global Functions
+function declareWin() {
+    alert('You Win!');
+    wins++;
+    $('.wins').text(wins);
+};
+function declareLoss() {
+    alert('You Lose');
+    losses++;
+    $('.losses').text(losses);
+};
+function reset() {
+    let randNumberString = [Math.floor(Math.random() * 31) + 30];
+    let randNumber = parseInt(randNumberString);
+    let playerNumberString = 0;
+    let playerNumber=parseInt(playerNumberString);
+    let imageNumber1string = [Math.floor(Math.random() * 11) + 1];
+    let imageNumber2string = [Math.floor(Math.random() * 11) + 1];
+    let imageNumber3string = [Math.floor(Math.random() * 11) + 1];
+    let imageNumber4string = [Math.floor(Math.random() * 11) + 1];
+    $('.yourNumber').text(playerNumber);
+    $('.selectedNumber').text(randNumber);
+};
 
 //on Click Events And Populate to Your Number
 
+$('.image1').on('click', function (event) {
+    playerNumber = playerNumber + imageNumber1;
+    $('.yourNumber').text(playerNumber);
+    if (playerNumber > randNumber) {
+        declareLoss();
+        reset();
+    }
+    else if (playerNumber == randNumber) {
+        declareWin();
+        reset();
+    };
+});
+$('.image2').on('click', function (event) {
+    playerNumber = playerNumber + imageNumber2;
+    $('.yourNumber').text(playerNumber);
+    if (playerNumber > randNumber) {
+        declareLoss();
+        reset();
+    }
+    else if (playerNumber == randNumber) {
+        declareWin();
+        reset();
+    };
+});
+$('.image3').on('click', function (event) {
+    playerNumber = playerNumber + imageNumber3;
+    $('.yourNumber').text(playerNumber);
+    if (playerNumber > randNumber) {
+        declareLoss();
+        reset();
+    }
+    else if (playerNumber == randNumber) {
+        declareWin();
+        reset();
+    };
+});
+$('.image4').on('click', function (event) {
+    playerNumber = playerNumber + imageNumber4;
+    $('.yourNumber').text(playerNumber);
+    if (playerNumber > randNumber) {
+        declareLoss();
+        reset();
+    }
+    else if (playerNumber == randNumber) {
+        declareWin();
+        reset();
+    };
+})
 
+
+
+});
 //Winning and Losing Conditions and Alerts
