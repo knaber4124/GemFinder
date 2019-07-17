28708516b6e7012cd4bd0e1e74f9de0a9bced6ed
Keith Naber <knaber4124@gmail.com>
2019-07-17 07:55:54 -0400

    added win and loss windows

--- gem-game.js
+++ gem-game.js
@@ -1,101 +1,105 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-//Global Arrays
-let randNumberString = [Math.floor(Math.random() * 31) + 30];
-let playerNumberString = 0;
-let wins = 0;
-let losses = 0;
-let randNumber = parseInt(randNumberString);
-let imageNumber1string = [Math.floor(Math.random() * 11) + 1];
-let imageNumber2string = [Math.floor(Math.random() * 11) + 1];
-let imageNumber3string = [Math.floor(Math.random() * 11) + 1];
-let imageNumber4string = [Math.floor(Math.random() * 11) + 1];
-let playerNumber = parseInt(playerNumberString);
-
-//Assign Numbers To Images
-let imageNumber1 = parseInt(imageNumber1string);
-let imageNumber2 = parseInt(imageNumber2string);
-let imageNumber3 = parseInt(imageNumber3string);
-let imageNumber4 = parseInt(imageNumber4string);
-
-//DOM Manipulation
-$('.yourNumber').text(playerNumber);
-$('.selectedNumber').text(randNumber);
-$('.wins').text(wins);
-$('.losses').text(losses);
-
-//Global Functions
-function declareWin() {
-    alert('You Win!');
-    wins++;
-    $('.wins').text(wins);
-};
-function declareLoss() {
-    alert('You Lose');
-    losses++;
-    $('.losses').text(losses);
-};
-function reset() {
+    //Global Arrays
     let randNumberString = [Math.floor(Math.random() * 31) + 30];
-    let randNumber = parseInt(randNumberString);
     let playerNumberString = 0;
-    let playerNumber=parseInt(playerNumberString);
+    let wins = 0;
+    let losses = 0;
+    let randNumber = parseInt(randNumberString);
     let imageNumber1string = [Math.floor(Math.random() * 11) + 1];
     let imageNumber2string = [Math.floor(Math.random() * 11) + 1];
     let imageNumber3string = [Math.floor(Math.random() * 11) + 1];
     let imageNumber4string = [Math.floor(Math.random() * 11) + 1];
+    let playerNumber = parseInt(playerNumberString);
+
+    //Assign Numbers To Images
+    let imageNumber1 = parseInt(imageNumber1string);
+    let imageNumber2 = parseInt(imageNumber2string);
+    let imageNumber3 = parseInt(imageNumber3string);
+    let imageNumber4 = parseInt(imageNumber4string);
+
+    //DOM Manipulation
     $('.yourNumber').text(playerNumber);
     $('.selectedNumber').text(randNumber);
-};
+    $('.wins').text(wins);
+    $('.losses').text(losses);
 
-//on Click Events, Populate to Your Number, and check for Win/Loss
+    //Global Functions
 
-$('.image1').on('click', function (event) {
-    playerNumber = playerNumber + imageNumber1;
-    $('.yourNumber').text(playerNumber);
-    if (playerNumber > randNumber) {
-        declareLoss();
-        reset();
-    }
-    else if (playerNumber == randNumber) {
-        declareWin();
-        reset();
+    function reset() {
+        randNumberString = [Math.floor(Math.random() * 31) + 30];
+        randNumber = parseInt(randNumberString);
+        playerNumberString = 0;
+        playerNumber = parseInt(playerNumberString);
+        imageNumber1string = [Math.floor(Math.random() * 11) + 1];
+        imageNumber2string = [Math.floor(Math.random() * 11) + 1];
+        imageNumber3string = [Math.floor(Math.random() * 11) + 1];
+        imageNumber4string = [Math.floor(Math.random() * 11) + 1];
+        imageNumber1 = parseInt(imageNumber1string);
+        imageNumber2 = parseInt(imageNumber2string);
+        imageNumber3 = parseInt(imageNumber3string);
+        imageNumber4 = parseInt(imageNumber4string);
+        $('.yourNumber').text(playerNumber);
+        $('.selectedNumber').text(randNumber);
     };
-});
-$('.image2').on('click', function (event) {
-    playerNumber = playerNumber + imageNumber2;
-    $('.yourNumber').text(playerNumber);
-    if (playerNumber > randNumber) {
-        declareLoss();
-        reset();
-    }
-    else if (playerNumber == randNumber) {
-        declareWin();
-        reset();
+    function declareWin() {
+        wins++;
+        $('.wins').text(wins);
+        window.open('./win.html');
     };
-});
-$('.image3').on('click', function (event) {
-    playerNumber = playerNumber + imageNumber3;
-    $('.yourNumber').text(playerNumber);
-    if (playerNumber > randNumber) {
-        declareLoss();
-        reset();
-    }
-    else if (playerNumber == randNumber) {
-        declareWin();
-        reset();
+    function declareLoss() {
+        losses++;
+        $('.losses').text(losses);
+        window.open('./loss.html');
     };
-});
-$('.image4').on('click', function (event) {
-    playerNumber = playerNumber + imageNumber4;
-    $('.yourNumber').text(playerNumber);
-    if (playerNumber > randNumber) {
-        declareLoss();
-        reset();
-    }
-    else if (playerNumber == randNumber) {
-        declareWin();
-        reset();
-    };
-})
+    //on Click Events, Populate to Your Number, and check for Win/Loss
+
+    $('.image1').on('click', function (event) {
+        playerNumber = playerNumber + imageNumber1;
+        $('.yourNumber').text(playerNumber);
+        if (playerNumber > randNumber) {
+            declareLoss();
+            reset();
+        }
+        else if (playerNumber == randNumber) {
+            declareWin();
+            reset();
+        };
+    });
+    $('.image2').on('click', function (event) {
+        playerNumber = playerNumber + imageNumber2;
+        $('.yourNumber').text(playerNumber);
+        if (playerNumber > randNumber) {
+            declareLoss();
+            reset();
+        }
+        else if (playerNumber == randNumber) {
+            declareWin();
+            reset();
+        };
+    });
+    $('.image3').on('click', function (event) {
+        playerNumber = playerNumber + imageNumber3;
+        $('.yourNumber').text(playerNumber);
+        if (playerNumber > randNumber) {
+            declareLoss();
+            reset();
+        }
+        else if (playerNumber == randNumber) {
+            declareWin();
+            reset();
+        };
+    });
+    $('.image4').on('click', function (event) {
+        playerNumber = playerNumber + imageNumber4;
+        $('.yourNumber').text(playerNumber);
+        if (playerNumber > randNumber) {
+            declareLoss();
+            reset();
+        }
+        else if (playerNumber == randNumber) {
+            declareWin();
+            reset();
+        };
+    })
 });
